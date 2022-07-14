37c6b591b44cfad414adf3ca91a27a41ff7d4ac7
kennethryandy <dykennethryan@gmail.com>
2022-07-14 21:20:42 +0800

    add contact emailjs and update contact

--- src/components/Contact/Form/form.jsx
+++ src/components/Contact/Form/form.jsx
@@ -1,9 +1,13 @@
-import React, { useLayoutEffect } from 'react';
+import React, { useLayoutEffect, useState } from 'react';
+import emailjs from 'emailjs-com';
 import useArrayRefs from '../../../hooks/useArrayRefs';
 import Input from '../../input';
 // MUI
 import Box from '@mui/material/Box';
 import { FormStyled, ContactButton } from './formStyled';
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
+import Dialog from '../../dialog';
 
 const inputStyled = {
 	"& .MuiInputLabel-root:not(.Mui-focused)": { color: "text.primary" },
@@ -14,30 +18,65 @@ const inputStyled = {
 
 const Form = ({ tl, delay }) => {
 	const [refs, setRefs] = useArrayRefs();
+	const [openDia, setOpenDia] = useState(false);
+	const [openBD, setOpenBD] = useState(false);
 
 	useLayoutEffect(() => {
 		if (tl) {
 			tl.from(refs.current, { y: 16, opacity: 0, stagger: 0.1, delay: delay ? delay : 0 }, 'start-contact')
 		}
 		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [tl, refs])
+	}, [tl, refs]);
+
+	function handleSubmit (e) {
+		e.preventDefault();
+		setOpenBD(true);
+		emailjs.sendForm('service_wfa0yft', 'template_cew8hbg', e.currentTarget, 'user_lwR1O0HcFGB2BCnsNBfVw')
+			.then((result) => {
+				handleCloseBD();
+				console.log(result.text);
+				setOpenDia(true);
+				e.target.reset();
+			}, (error) => {
+				handleCloseBD();
+				e.target.reset();
+				console.log(error.text);
+			});
+	}
+
+	function handleCloseDialog () {
+		setOpenDia(false)
+	}
+
+	function handleCloseBD () {
+		setOpenBD(false);
+	}
 
 	return (
-		<FormStyled>
-			<Box sx={{ display: "flex", gap: 2 }}>
-				<Input ref={setRefs} margin="dense" sx={inputStyled} label="Name" variant="standard" color="secondary" fullWidth />
-				<Input ref={setRefs} margin="dense" sx={inputStyled} label="Email" variant="standard" color="secondary" fullWidth />
-			</Box>
-			<Box>
-				<Input ref={setRefs} margin="dense" sx={inputStyled} label="Subject" variant="standard" color="secondary" fullWidth />
-			</Box>
-			<Box>
-				<Input ref={setRefs} margin="dense" sx={inputStyled} label="Message" variant="standard" color="secondary" fullWidth multiline minRows={3} />
-			</Box>
-			<Box>
-				<ContactButton ref={setRefs} variant="outlined" color="secondary">Hit me up!</ContactButton>
-			</Box>
-		</FormStyled>
+		<>
+			<FormStyled onSubmit={handleSubmit} autoComplete="off">
+				<Box sx={{ display: "flex", gap: 2 }}>
+					<Input ref={setRefs} margin="dense" sx={inputStyled} label="Name" name="name" required variant="standard" color="secondary" fullWidth />
+					<Input ref={setRefs} margin="dense" sx={inputStyled} label="Email" name="email" required type="email" variant="standard" color="secondary" fullWidth />
+				</Box>
+				<Box>
+					<Input ref={setRefs} margin="dense" sx={inputStyled} label="Subject" name="subject" required variant="standard" color="secondary" fullWidth />
+				</Box>
+				<Box>
+					<Input ref={setRefs} margin="dense" sx={inputStyled} label="Message" name="message" required variant="standard" color="secondary" fullWidth multiline minRows={3} />
+				</Box>
+				<Box>
+					<ContactButton ref={setRefs} variant="outlined" type="submit" color="secondary">Hit me up!</ContactButton>
+				</Box>
+			</FormStyled>
+			<Dialog open={openDia} handleClose={handleCloseDialog} />
+			<Backdrop
+				sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+				open={openBD}
+			>
+				<CircularProgress color="inherit" />
+			</Backdrop>
+		</>
 	)
 }
 
---
+++ src/components/dialog.jsx
@@ -0,0 +1,96 @@
+import React, { forwardRef } from 'react';
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import CheckIcon from '@mui/icons-material/Check';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import MuiDialog from '@mui/material/Dialog';
+import Slide from '@mui/material/Slide';
+
+const Transition = forwardRef(function Transition (props, ref) {
+	return <SlideStyled direction="down" ref={ref} {...props} />;
+});
+
+
+const SlideStyled = styled(Slide)`
+	animation: fade 225ms cubic-bezier(0, 0, 0.2, 1);
+`;
+
+
+const DialogHeader = styled('div')(({ theme }) => ({
+	width: '100%',
+	textAlign: 'center',
+}));
+
+const DialogIcon = styled('div')(({ theme }) => ({
+	backgroundColor: theme.palette.secondary.dark,
+	borderRadius: '50%',
+	position: 'absolute',
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'center',
+	width: '6em',
+	height: '6em',
+	left: '50%',
+	top: '-60px',
+	boxShadow: theme.shadows[2],
+	transform: 'translateX(-50%)',
+	"&>.MuiSvgIcon-root": {
+		color: "#fff",
+		width: '3em',
+		height: '3em',
+	}
+}));
+
+const Dialog = ({ open, handleClose }) => {
+	return (
+		<>
+			<MuiDialog
+				maxWidth="sm"
+				open={open}
+				onClose={handleClose}
+				TransitionComponent={Transition}
+				keepMounted
+				aria-labelledby="alert-dialogcontacttitle"
+				aria-describedby="alert-dialog-description"
+				sx={{
+					"& .MuiPaper-root": {
+						overflow: 'visible',
+						position: 'relative',
+						paddingX: 3
+					}
+				}}
+			>
+				<DialogHeader>
+					<DialogIcon>
+						<CheckIcon />
+					</DialogIcon>
+					<DialogTitle id="alert-contact-title" sx={{ paddingTop: 7 }}>
+						Awesome!
+					</DialogTitle>
+				</DialogHeader>
+				<DialogContent>
+					<DialogContentText id="alert-contact-description">
+						Your message has been successfully sent.
+					</DialogContentText>
+				</DialogContent>
+				<DialogActions sx={{ paddingX: 3, paddingBottom: 3 }}>
+					<Button sx={{
+						fontSize: '1rem',
+						color: '#fff',
+						backgroundColor: 'secondary.dark',
+						fontFamily: 'Fira Code',
+						'&:hover': {
+							color: 'secondary.dark',
+							backgroundColor: 'secondary.light',
+						}
+					}} fullWidth variant="contained" onClick={handleClose}>Ok</Button>
+				</DialogActions>
+			</MuiDialog>
+		</>
+	)
+}
+
+export default Dialog;
